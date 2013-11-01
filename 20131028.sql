b44cd9302f48595726c262b2d31da2bca65ebaa3
Leejaejin <starcx@naver.com>
2013-11-01 14:49:46 +0900

    20131031

---
+++ 20131028.sql
@@ -0,0 +1,39 @@
+﻿SELECT B.meter_sn, 
+       B.stand_dt, 
+       COALESCE(A.use_amt_fa, 0) AS USE_AMT_FA 
+FROM   (SELECT A.meter_sn, 
+               A.stand_dt, 
+               A.time_cd, 
+               Sum(use_amt_fa) AS USE_AMT_FA 
+        FROM   t_mr_meter_bytm_sum A 
+        WHERE  1 = 1 
+               AND A.time_cd = 'H01' 
+               AND A.meter_sn IN ( '000000000039' ) 
+               AND A.stand_dt BETWEEN '20131028' AND '20131028' 
+        GROUP  BY A.meter_sn, 
+                  A.stand_dt, 
+                  A.time_cd 
+        ORDER  BY A.meter_sn, 
+                  A.stand_dt) A 
+       RIGHT OUTER JOIN (SELECT meter_sn, 
+                                To_char( '20131028':: date - i, 'yyyymmdd') AS STAND_DT, 
+                                A.time_cd 
+                         FROM   Generate_series(0, (SELECT '20131028':: date - '20131028':: date)) 
+                                AS t(i 
+                                ), 
+                                (SELECT A.meter_sn, 
+                                        B.time_cd 
+                                 FROM   t_mr_meter_bytm_sum A, 
+                                        t_time_cd B 
+                                 WHERE  meter_sn IN ( '000000000039' ) 
+                                        AND A.time_cd = B.time_cd 
+                                        AND B.cd_type = 'H01' 
+                                 GROUP  BY A.meter_sn, 
+                                           B.time_cd) A 
+                         ORDER  BY meter_sn, 
+                                   stand_dt) B 
+                     ON B.meter_sn = A.meter_sn 
+                        AND B.stand_dt = A.stand_dt 
+                        AND A.time_cd = B.time_cd 
+ORDER  BY B.meter_sn, 
+          B.stand_dt 
---
+++ 20131029.sql
@@ -0,0 +1,46 @@
+﻿SELECT B.stand_dt, 
+       B.time_cd, 
+       COALESCE(A.use_amt_fa, 0)   AS USE_AMT_FA, 
+       Substr(B.start_tm, 1, 2) 
+       || ':' 
+       || Substr(B.start_tm, 3, 4) AS START_TM, 
+       Substr(B.end_tm, 1, 2) 
+       || ':' 
+       || Substr(B.end_tm, 3, 4)   AS END_TM 
+FROM   (SELECT B.time_cd, 
+               B.view_odr, 
+               A.mtr_date                AS STAND_DT, 
+               B.start_tm, 
+               B.end_tm, 
+               Sum(A.lghtn_cctp_cns_qnt) AS USE_AMT_FA 
+        FROM   t_obdng_by_purp_bytm_sum A, 
+               t_time_cd B, 
+               t_obdng_info_bas C 
+        WHERE  A.mtr_date BETWEEN '20131028' AND '20131028' 
+               AND A.obdng_id = C.obdng_id 
+               AND C.rgn_cd = '01020000' 
+               AND A.time_cd = B.time_cd 
+               AND C.obdng_id = 'A01-B0000000896'
+        GROUP  BY A.mtr_date, 
+                  B.time_cd, 
+                  B.view_odr, 
+                  B.start_tm, 
+                  B.end_tm 
+        ORDER  BY A.mtr_date, 
+                  B.view_odr) A 
+       RIGHT OUTER JOIN (SELECT i, 
+                                To_char( '20131028':: date - i, 'yyyymmdd') AS STAND_DT, 
+                                start_tm, 
+                                end_tm, 
+                                time_cd 
+                         FROM   Generate_series(0, (SELECT '20131028':: date - '20131028':: date)) 
+                                AS t(i 
+                                ) 
+                                INNER JOIN t_time_cd 
+                                        ON cd_type = 'H01' 
+                         ORDER  BY stand_dt, 
+                                   start_tm) B 
+                     ON B.stand_dt = A.stand_dt 
+                        AND B.start_tm = A.start_tm 
+ORDER  BY B.stand_dt, 
+          B.start_tm 
